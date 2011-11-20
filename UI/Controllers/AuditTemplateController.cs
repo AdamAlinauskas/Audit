d2b204a6e23d2a70cc5aa1dd32b894c8f927024c
Adam Alinauskas <aalinauskas@gmail.com>
2011-11-20 15:53:02 -0700

    Stubbing out the listing view and create view.
    Added MVC contrib for Html methods that use generics.
    Added nunit.

---
+++ UI/Controllers/AuditTemplateController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class AuditTemplateController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Create()
+        {
+            return View();
+        }
+    }
+}
